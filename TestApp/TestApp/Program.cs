b00aed25868c98babd520d4a8ca3bcd92998fe5a
Grom201 <40233575+Grom201@users.noreply.github.com>
2020-11-28 16:00:38 +0200

    Initial commit

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("Day" +i);
+                i++;
+            }
+            while (i<31);
+
+
+
+        }
+    }
+}
